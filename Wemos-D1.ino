98b8d038ffce5dfa9a94f0cb6162b6f1733de3e8
Giumbe <160049975+Giumbe@users.noreply.github.com>
2024-03-22 11:27:49 +0100

    Add files via upload

---
+++ Wemos-D1.ino
@@ -0,0 +1,40 @@
+#include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+
+SoftwareSerial mySoftwareSerial(3, 1); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+
+const int pirPin = 17;
+boolean presenceDetected = false;
+
+void setup() {
+  Serial.begin(9600);
+  mySoftwareSerial.begin(9600);
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1. Please recheck the connection!"));
+    Serial.println(F("2. Please insert the SD card!"));
+    while (true);
+  }
+
+  myDFPlayer.volume(25); // Imposta il volume desiderato
+}
+
+void loop() {
+  Serial.println("A");
+  if (digitalRead(pirPin) == HIGH && !presenceDetected) {
+    presenceDetected = true;
+    playRandomTrack();
+  } else if (digitalRead(pirPin) == LOW && presenceDetected) {
+    presenceDetected = false;
+    myDFPlayer.stop(); // Ferma la riproduzione
+  }
+}
+
+void playRandomTrack() {
+  myDFPlayer.randomAll(); // Riproduci traccia casuale
+  delay(15000); // Attendi * secondi prima di verificare la disponibilità
+  //Serial.println(F("B"));
+
+}
